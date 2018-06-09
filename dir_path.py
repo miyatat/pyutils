fe13056195397f761f619f13a96f0dc8a6d6e138
Unknown <takuya5589@yahoo.co.jp>
2018-06-09 10:56:58 +0900

    add new utils

---
+++ dir_path.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+import os
+
+logger = getLogger(__name__)
+
+SRC_DIR_NAME = os.path.dirname(os.path.abspath(__file__))
+
+def join_and_makedirs(*keys):
+    path_name = ''
+    for key in keys:
+        path_name = os.path.join(path_name, key)
+    os.makedirs(path_name, exist_ok=True)
+    return path_name
+
+
+
+
+if __name__ == '__main__':
+    pass

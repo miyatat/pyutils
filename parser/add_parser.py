2e8c2ac54804fbf30665f23e6411fb6b8056ec8d
Takuya Miyata <makestein.m@gmail.com>
2018-06-26 17:42:02 +0900

    add parser added

--- parser/add_parser.py
+++ parser/add_parser.py
@@ -1,54 +1,95 @@
 # -*- coding: utf-8 -*-
 
 from logging import getLogger
+import argparse
 
 logger = getLogger(__name__)
 
 
-def add_data_name(parser):
+def get_parser(description):
+    parser = argparse.ArgumentParser(description=description)
+    return parser
+
+
+def add_path_root_src(parser):
     """
-    data_nameを引数として取るためのparserを追加するための関数
+    path_root_src is needed.
     Args:
         parser (argparse.ArgumentParser): parser instance from Argparse
-
     Returns:
         added parser instance
     """
     parser.add_argument(
-        'data_name',
+        'path_root_src',
         action='store',
         nargs=None,
         const=None,
         default=None,
         type=str,
         choices=None,
-        help='data_name detector.',
+        help='Directory path where your taken photo files are located.',
         metavar=None
     )
     return parser
 
 
-def add_img_api_type(parser):
+def add_path_root_dst(parser):
     """
-    img_api_typeを引数として取るためのparserを追加するための関数
+    path-root-dst is optional
     Args:
         parser (argparse.ArgumentParser): parser instance from Argparse
-
     Returns:
         added parser instance
     """
     parser.add_argument(
-        'img_api_type',
+        '-d', '--path-root-dst',
         action='store',
-        nargs=None,
+        nargs='?',
         const=None,
-        default='google',
+        default=None,
         type=str,
         choices=None,
-        help='"google" or "bing". Image API type what you want to use.',
+        help='Directory path where you want to create date folder and locate photo files.'
+             ' (default: same as source directory)',
         metavar=None
     )
     return parser
+
+
+def add_extensions(parser):
+    """
+    extensions is optional. 2 or more ok. Default 'jpg'
+    Args:
+        parser (argparse.ArgumentParser): parser instance from Argparse
+    Returns:
+        added parser instance
+    """
+    parser.add_argument(
+        '-e', '--sort-files-extentions',
+        action='store',
+        nargs='+',
+        const=None,
+        default=['jpg'],
+        type=str,
+        choices=None,
+        help='Extentions of file which you want to sort. (default: jpg)',
+        metavar=None
+    )
+    return parser
+
+
+def add_debug_mode(parser):
+    """
+    debug option.
+    Args:
+        parser:
+    """
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        default=False,
+        help='debug mode if this flag is set (default: False)'
+    )
 
 
 if __name__ == '__main__':
